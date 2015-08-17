e881e8ba92b976c2a5bf162c14320ba91e80e3a5
zak1019010 <zak1019010@yahoo.com>
2015-08-17 11:49:14 -0400

    Caster’s Caravan website

---
+++ iq.php
@@ -0,0 +1,26 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+<?php
+    $email_from = 'yourname@yourwebsite.com';
+ 
+    $email_subject = "New Form submission";
+ 
+    $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message";
+?>
+
+<?php
+ 
+  $to = "yourname@yourwebsite.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+ ?>
